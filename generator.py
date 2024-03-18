590663328f39a6d0bed1df8ebeef97cdf2efdde1
Chantelle Osafo <84527073+chantellecv@users.noreply.github.com>
2024-03-18 16:13:30 +0000

    Update file

--- generator.py
+++ generator.py
@@ -7,23 +7,24 @@ def api_function(instructions):
         "accept": "application/json"
     }
     api_response = requests.post('https://text-named-er-spacy.icysmoke-f4846de1.switzerlandnorth.azurecontainerapps.io/api/v1/entity-recognition', json={"text": instructions}, headers=headers)
+    
     if api_response.status_code == 200:
         results_str = " "
-        entity_list = api_response["response"].strip('[]').split(', ')
+        entity_list = api_response.json()["response"].strip('[]').split(', ')
         count = 1
-        
+    
         for entity in entity_list:
             results_str += str(count) + ". " + entity + "\n"
             count += 1
         return results_str
 
-st.title("Named Entity Recognition App")
+st.title("Entity Recognition App")
+text = st.text_area("Enter your text here:")
 
-text_input = st.text_area("Enter your text:")
-if st.button("Name Entities"):
-    if text_input:
-        named_entities = api_function(text_input)
-        st.write("Named Entities:")
-        st.write(named_entities)
+if st.button("Identify Entities"):
+    if text:
+        entities = api_function(text)
+        st.write("Entities identified in the text:")
+        st.write(entities)
     else:
-        st.write("Please enter some text.")
+        st.warning("Please enter some text to identify entities.")
