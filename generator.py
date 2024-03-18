ef7c4693273a500e0ffc75f0929503d54a5672fd
Chantelle Osafo <84527073+chantellecv@users.noreply.github.com>
2024-03-18 12:53:06 +0000

    Update file

--- generator.py
+++ generator.py
@@ -5,11 +5,11 @@ def main():
 
     number = st.number_input("Enter a number:", step=1)
 
-    if number % 10 == 0:
-        st.write(f"{number} is divisible by 10")
+    if number % 15 == 0:
+        st.write(f"{number} is divisible by 15")
         st.write("YES IT WORKS")
     else:
-        st.write(f"{number} is not divisible by 10")
+        st.write(f"{number} is not divisible by 15")
 
 if __name__ == '__main__':
     main()
