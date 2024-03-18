f0be14f3837743d2d128e0536bfc892528ebc7b6
Chantelle Osafo <84527073+chantellecv@users.noreply.github.com>
2024-03-18 15:55:38 +0000

    Update file

--- generator.py
+++ generator.py
@@ -1,25 +1,29 @@
 import streamlit as st
 import requests
 
-# Function to call the API and get the text summary
 def api_function(instructions):
     headers = {
         "Content-Type": "application/json",
         "accept": "application/json"
     }
-    api_response = requests.post('https://text-summarization.agreeabledune-08a9cefb.switzerlandnorth.azurecontainerapps.io/api/v1/classify', json={"text": instructions}, headers=headers)
+    api_response = requests.post('https://text-named-er-spacy.icysmoke-f4846de1.switzerlandnorth.azurecontainerapps.io/api/v1/entity-recognition', json={"text": instructions}, headers=headers)
     if api_response.status_code == 200:
-        return api_response.json()[0]["answer"]
+        results_str = " "
+        entity_list = api_response["response"].strip('[]').split(', ')
+        count = 1
+        
+        for entity in entity_list:
+            results_str += str(count) + ". " + entity + "\n"
+            count += 1
+        return results_str
 
-# Streamlit app UI
-st.title("Text Summarization App")
+st.title("Named Entity Recognition App")
 
-text_input = st.text_area("Enter the text you want to summarize:")
-if st.button("Summarize"):
+text_input = st.text_area("Enter your text:")
+if st.button("Name Entities"):
     if text_input:
-        summary = api_function(text_input)
-        st.header("Summary:")
-        st.write(summary)
-        st.write("SUMMARY DONE")
+        named_entities = api_function(text_input)
+        st.write("Named Entities:")
+        st.write(named_entities)
     else:
-        st.warning("Please enter some text to summarize.")
+        st.write("Please enter some text.")
