59d62022876188903939e4e144905c5de286b00c
Chantelle Osafo <84527073+chantellecv@users.noreply.github.com>
2024-03-18 16:20:20 +0000

    Update file

--- generator.py
+++ generator.py
@@ -7,16 +7,35 @@ def api_function(instructions):
         "accept": "application/json"
     }
     api_response = requests.post('https://text-summarization.agreeabledune-08a9cefb.switzerlandnorth.azurecontainerapps.io/api/v1/classify', json={"text": instructions}, headers=headers)
+    
     if api_response.status_code == 200:
-        return api_response.json()[0]["answer"]
-
-# Streamlit app
-st.title("Text Summarizer App")
-text = st.text_area("Enter the text you want to summarize:")
-if st.button("Summarize"):
-    if text:
-        summary = api_function(text)
-        st.write("Summary:")
-        st.write(summary)
+        summarised_text = api_response.json()[0]["answer"]
+        
+        api_response_entities = requests.post('https://text-named-er-spacy.icysmoke-f4846de1.switzerlandnorth.azurecontainerapps.io/api/v1/entity-recognition', json={"text": summarised_text}, headers=headers)
+    
+        if api_response_entities.status_code == 200:
+            results_str = " "
+            entity_list = api_response_entities.json()["response"].strip('[]'').split('', '')
+            count = 1
+        
+            for entity in entity_list:
+                results_str += str(count) + ". " + entity + "
+"
+                count += 1
+            return summarised_text, results_str
+        else:
+            return "Error in identifying entities."
     else:
-        st.write("Please enter some text to summarize.")
+        return "Error in summarizing text."
+
+st.title("Text Summarization and Entity Recognition App")
+
+user_input = st.text_area("Enter text to summarize and identify entities:")
+
+if st.button("Summarize and Identify Entities"):
+    with st.spinner("Summarizing and identifying entities..."):
+        summary, entities = api_function(user_input)
+        st.subheader("Summarized Text:")
+        st.write(summary)
+        st.subheader("Identified Entities:")
+        st.write(entities)
