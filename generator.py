09344a54f39459648bbca2ff7f96573f5cac1c86
Chantelle Osafo <84527073+chantellecv@users.noreply.github.com>
2024-03-18 12:40:03 +0000

    Update file

--- generator.py
+++ generator.py
@@ -6,8 +6,8 @@ st.title("Number Divisibility Checker")
 # Create a text input for the user to enter a number
 number = st.number_input("Enter a number:")
 
-# Check if the number is divisible by 2
-if number % 2 == 0:
-    st.write(f"{number} is divisible by 2")
+# Check if the number is divisible by 3
+if number % 3 == 0:
+    st.write(f"{number} is divisible by 3")
 else:
-    st.write(f"{number} is not divisible by 2")
+    st.write(f"{number} is not divisible by 3")
