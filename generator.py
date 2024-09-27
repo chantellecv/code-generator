fe8c440c14bfd583c7239af2701f198600f16456
chantellecv <84527073+chantellecv@users.noreply.github.com>
2024-09-27 15:19:32 +0000

    groq + updates

--- generator.py
+++ generator.py
@@ -1,20 +1,27 @@
 import streamlit as st
-import math
+import requests
 
-def main():
-    st.title("Is my number odd or even?")
+def api_function(instructions):
+    headers = {
+        "Content-Type": "application/json",
+        "accept": "application/json"
+    }
+    api_response = requests.post('https://text-summarization.agreeabledune-08a9cefb.switzerlandnorth.azurecontainerapps.io/api/v1/classify', json={"text": instructions}, headers=headers)
+    if api_response.status_code == 200:
+        return api_response.json()[0]["answer"]
 
-    user_number = st.number_input("Enter a number:")
+st.title("Text Summarization App")
 
-    try:
-        user_number = float(user_number)
-    except ValueError:
-        st.error("Invalid input, please enter a number.")
-        return
+text1 = st.text_area("Enter Text 1:")
+text2 = st.text_area("Enter Text 2:")
 
-    result = "ODD" if math.floor(user_number) % 2 != 0 else "EVEN"
-
-    st.header(f"The number {user_number} is {result}.")
-
-if __name__ == "__main__":
-    main()
+if st.button("Summarize"):
+    if text1 and text2:
+        summary1 = api_function(text1)
+        summary2 = api_function(text2)
+        
+        st.write("Summary of Text 1:")
+        st.write(summary1)
+        
+        st.write("Summary of Text 2:")
+        st.write(summary2)
