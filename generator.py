510ab04f690f87e48c98a5160ac71566b0dac5b6
Chantelle Osafo <84527073+chantellecv@users.noreply.github.com>
2024-09-27 17:38:08 +0000

    Update file

--- generator.py
+++ generator.py
@@ -1,14 +1,11 @@
 import streamlit as st
 
-def is_odd(n):
-    return n % 2 != 0
-
 st.title("Odd Number Checker")
 
 num = st.number_input("Enter a number:", step=1)
 
 if st.button("Check"):
-    if is_odd(num):
+    if num % 2 != 0:
         st.success(f"{num} is an odd number!")
     else:
         st.error(f"{num} is not an odd number.")
