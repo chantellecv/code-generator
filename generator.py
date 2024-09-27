ca2b0c8e0e8362345ed888db5ebeafc451c457f0
Chantelle Osafo <84527073+chantellecv@users.noreply.github.com>
2024-09-27 15:34:49 +0000

    Update file

--- generator.py
+++ generator.py
@@ -1,27 +1,18 @@
 import streamlit as st
-import requests
+import re
 
-def api_function(instructions):
-    headers = {
-        "Content-Type": "application/json",
-        "accept": "application/json"
-    }
-    api_response = requests.post('https://text-summarization.agreeabledune-08a9cefb.switzerlandnorth.azurecontainerapps.io/api/v1/classify', json={"text": instructions}, headers=headers)
-    if api_response.status_code == 200:
-        return api_response.json()[0]["answer"]
+def main():
+    st.title("Word Counter")
+    
+    sentence = st.text_input("Enter a sentence:")
 
-st.title("Text Summarization App")
-
-text1 = st.text_area("Enter Text 1:")
-text2 = st.text_area("Enter Text 2:")
-
-if st.button("Summarize"):
-    if text1 and text2:
-        summary1 = api_function(text1)
-        summary2 = api_function(text2)
+    if not sentence:
+        st.error("Please enter a sentence.")
+    else:
+        words = re.split(r'\W+', sentence)
+        word_count = len(words)
         
-        st.write("Summary of Text 1:")
-        st.write(summary1)
-        
-        st.write("Summary of Text 2:")
-        st.write(summary2)
+        st.header(f"The sentence '{sentence}' contains {word_count} words.")
+
+if __name__ == "__main__":
+    main()
