f9a651443a7a6ca000dc9289d4dc94a3319fc3a1
Chantelle Osafo <84527073+chantellecv@users.noreply.github.com>
2024-09-27 15:40:25 +0000

    Update file

--- generator.py
+++ generator.py
@@ -1,18 +1,17 @@
 import streamlit as st
-import re
 
 def main():
-    st.title("Letter Counter")
+    st.title("Word Counter")
     
     sentence = st.text_input("Enter a sentence:")
 
     if not sentence:
         st.error("Please enter a sentence.")
     else:
-        letters = re.sub(r'\W+', '', sentence).lower()
-        letter_count = len(letters)
+        words = sentence.split()
+        word_count = len(words)
         
-        st.header(f"The sentence '{sentence}' contains {letter_count} letters.")
+        st.header(f"The sentence '{sentence}' contains {word_count} words.")
 
 if __name__ == "__main__":
     main()
