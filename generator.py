bf7337471195f6c559265c827924f29e54164923
Chantelle Osafo <84527073+chantellecv@users.noreply.github.com>
2024-09-27 17:38:38 +0000

    Update file

--- generator.py
+++ generator.py
@@ -8,6 +8,6 @@ if st.button("Check"):
     if num % 2 != 0:
         st.success(f"YESSSSS{num} is an odd number!")
     else:
-        st.error(f"{num} is not an odd number.")
+        st.error(f"NOOOO {num} is not an odd number.")
 
 st.stop()
