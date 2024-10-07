53cf6e8a40b3364c3b0e4fbb3d8e75996ffdc1cb
Chantelle Osafo <84527073+chantellecv@users.noreply.github.com>
2024-10-07 19:43:13 +0000

    Update file

--- generator.py
+++ generator.py
@@ -2,10 +2,10 @@ import streamlit as st
 
 def main():
     # Set the title of the app
-    st.title("Sum of Two Numbers")
+    st.title("Difference of Two Numbers")
 
     # Add a header to the app
-    st.header("Enter two numbers to calculate the sum:")
+    st.header("Enter two numbers to calculate the difference:")
 
     # Create a text input for the first number
     num1 = st.number_input("Enter the first number:", value=0.0)
@@ -15,8 +15,14 @@ def main():
 
     # Create a button to trigger the calculation
     if st.button("Calculate"):
-        result = num1 + num2
-        st.markdown(f"The sum of {num1} and {num2} is: **{result}**")
+        if num2 > num1:
+            result = num2 - num1
+            st.markdown(f"The difference between {num1} and {num2} is: **{result}**")
+        elif num1 > num2:
+            result = num1 - num2
+            st.markdown(f"The difference between {num2} and {num1} is: **{result}**")
+        else:
+            st.markdown(f"The difference between {num1} and {num2} is: **0**")
 
 if __name__ == "__main__":
     main()
