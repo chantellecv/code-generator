9c6bf266c5463a17eadc8ac91d47f15e9917a647
Chantelle Osafo <84527073+chantellecv@users.noreply.github.com>
2024-10-07 09:24:14 +0000

    Update file

--- generator.py
+++ generator.py
@@ -1,17 +1,20 @@
 import streamlit as st
 
-def calculate_sum(num1, num2):
-    return num1 + num2
+def calculate_difference(num1, num2):
+    if num1 >= num2:
+        return num1 - num2
+    else:
+        return num2 - num1
 
 def main():
-    st.title("Sum Calculator")
+    st.title("Difference Calculator")
 
     num1 = st.number_input("Enter the first number:")
     num2 = st.number_input("Enter the second number:")
 
-    sum_result = calculate_sum(num1, num2)
+    difference_result = calculate_difference(num1, num2)
 
-    st.write("The sum of", num1, "and", num2, "is", sum_result)
+    st.write("The difference between", num1, "and", num2, "is", difference_result)
 
 if __name__ == "__main__":
     main()
