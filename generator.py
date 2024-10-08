93cb946e6c501041b8ff827e98a99e9ef47d06fe
Chantelle Osafo <84527073+chantellecv@users.noreply.github.com>
2024-10-08 10:59:25 +0000

    Update file

--- generator.py
+++ generator.py
@@ -1,30 +1,16 @@
 import streamlit as st
-import math
 
-def is_prime(num):
-    if num <= 1:
-        return False
-    if num == 2:
-        return True
-    if num % 2 == 0:
-        return False
-    sqrt_num = int(math.sqrt(num))
-    for i in range(3, sqrt_num + 1, 2):
-        if num % i == 0:
-            return False
-    return True
-
-def app():
-    st.title("Prime Number Finderrrrrr")
+def even_numbers(app):
+    st.title("Even Number Finder")
     numbers = st.text_input("Enter a list of numbers (separated by ','): ")
     if numbers:
         try:
             numbers = list(map(int, numbers.split(',')))
-            prime_numbers = [num for num in numbers if is_prime(num)]
-            st.write("Prime numbers are: ")
-            st.write(prime_numbers)
+            even_numbers = [num for num in numbers if num % 2 == 0]
+            st.write("Even numbers are: ")
+            st.write(even_numbers)
         except ValueError:
             st.write("Invalid input. Please enter a list of integers separated by commas.")
 
 if __name__ == '__main__':
-    app()
+    even_numbers()
