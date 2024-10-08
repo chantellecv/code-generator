482a7456737d82d079cd8a4ff1d2ff9269dc4ab1
Chantelle Osafo <84527073+chantellecv@users.noreply.github.com>
2024-10-08 13:58:53 +0000

    Update file

--- generator.py
+++ generator.py
@@ -1,27 +1,12 @@
 import streamlit as st
-import math
 
-def is_prime(n):
-    if n <= 1:
-        return False
-    if n == 2:
-        return True
-    if n % 2 == 0:
-        return False
-    max_divisor = math.floor(math.sqrt(n))
-    for d in range(3, 1 + max_divisor, 2):
-        if n % d == 0:
-            return False
-    return True
+st.title("Even Number Checker")
+st.write("Enter a number to check if it's even:")
 
-st.title("Prime Number Checker")
-st.write("Enter a number to check if it's prime:")
+num_input = st.number_input("Number:", min_value=0, key="num_input")
 
-num_str = 'num_input'
-num = st.number_input("Number:", min_value=1, key=num_str)
-
-if st.button("Check", key=f"check_button"):
-    if is_prime(int(num)):
-        st.write(f"{num} is a prime number.")
+if st.button("Check", key="check_button"):
+    if num_input % 2 == 0:
+        st.write(f"{num_input} is an even number.")
     else:
-        st.write(f"{num} is not a prime number.")
+        st.write(f"{num_input} is an odd number.")
