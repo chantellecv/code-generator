5ab0d8c83cc900ee93cdbc6ab75f5e41bf26785f
Chantelle Osafo <84527073+chantellecv@users.noreply.github.com>
2024-10-08 13:58:18 +0000

    Update file

--- generator.py
+++ generator.py
@@ -1,12 +1,27 @@
 import streamlit as st
-def app():
-    st.title("Is the number odd?")
-    number = st.number_input("Enter a number: ")
+import math
 
-    if number % 2 != 0:
-        st.write(f"{number} is odd")
+def is_prime(n):
+    if n <= 1:
+        return False
+    if n == 2:
+        return True
+    if n % 2 == 0:
+        return False
+    max_divisor = math.floor(math.sqrt(n))
+    for d in range(3, 1 + max_divisor, 2):
+        if n % d == 0:
+            return False
+    return True
+
+st.title("Prime Number Checker")
+st.write("Enter a number to check if it's prime:")
+
+num_str = 'num_input'
+num = st.number_input("Number:", min_value=1, key=num_str)
+
+if st.button("Check", key=f"check_button"):
+    if is_prime(int(num)):
+        st.write(f"{num} is a prime number.")
     else:
-        st.write(f"{number} is even")
-
-if __name__ == "__main__":
-    app()
+        st.write(f"{num} is not a prime number.")
