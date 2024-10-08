1bc35d274d4cf719510f926ecdbec3f66f1e04f8
Chantelle Osafo <84527073+chantellecv@users.noreply.github.com>
2024-10-08 11:42:58 +0000

    Update file

--- generator.py
+++ generator.py
@@ -1,29 +1,15 @@
 import streamlit as st
-import pandas as pd
 
-# Data
-data = {
-    "Category": ["IT", "HR", "Marketing", "Sales", "IT", "HR", "Marketing", "Sales"],
-    "Department": ["Software Development", "Recruitment", "Digital Marketing", "Business Development", "Networking", "Employee Relations", "Content Creation", "Sales Support"],
-    "Priority": [1, 2, 3, 4, 5, 6, 7, 8]
-}
+def calculate_sum():
+    numbers_input = st.text_input("Enter numbers separated by commasss", value="")
+    if st.button("Submit"):
+        try:
+            numbers_list = [float(x) for x in numbers_input.split(',')]
+            result = sum(numbers_list)
+            st.header("Result")
+            st.write(f"The sum of the numbers is: {result}")
+        except ValueError:
+            st.error("Invalid input. Please enter numbers separated by commas.")
 
-df = pd.DataFrame(data)
-
-# Function to find the best department
-def find_best_department(df, category):
-    best_department = df.loc[(df["Category"] == category)].sort_values(by='Priority').head(1)
-    return best_department["Department"].values[0]
-
-# Create Streamlit App
-st.title("Department Finder")
-
-# Get user input
-category = st.selectbox("Select a category", sorted(df["Category"].unique()), key=("SELECT_CATEGORY"))
-
-# Check if the input is valid
-if category:
-    best_department = find_best_department(df, category)
-    st.write(f"The best department for {category} is {best_department}")
-else:
-    st.write("Please select a category.")
+if __name__ == "__main__":
+    calculate_sum()
