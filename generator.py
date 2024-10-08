f30e13f51e65cb5a3baa0d606c8a86fb3cfc195f
Chantelle Osafo <84527073+chantellecv@users.noreply.github.com>
2024-10-08 13:53:59 +0000

    Update file

--- generator.py
+++ generator.py
@@ -1,14 +1,14 @@
 import streamlit as st
 def app():
-    st.title("Is the number even?")
+    st.title("Is the number odd?")
     number = st.number_input("Enter a number: ")
 
     while True:
-        if number % 2 == 0:
-            st.write(f"{number} is even")
+        if number % 2 != 0:
+            st.write(f"{number} is odd")
             break
         else:
-            st.write(f"{number} is not even, please try again")
+            st.write(f"{number} is not odd, please try again.")
             number = int(st.text_input("Enter a new number: ", value=str(number)))
 
     st.write("Experiment complete, thank you!")
