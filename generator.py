6059e14c8c82ab9ee0630c2ffd09219581a69c5c
Chantelle Osafo <84527073+chantellecv@users.noreply.github.com>
2024-10-08 14:02:35 +0000

    Update file

--- generator.py
+++ generator.py
@@ -1,19 +1,19 @@
 import streamlit as st
 
-def check_if_even(num):
-    return num % 2 == 0
+def check_if_odd(num):
+    return num % 2 != 0
 
 def main():
-    st.title("Is a number even???")
+    st.title("Is a number odd?")
     
     num = st.number_input("Enter a number:")
     
-    if st.button("Check if even"):
-        is_even = check_if_even(num)
-        if is_even:
-            st.write(f"{num} is an even number.")
-        else:
+    if st.button("Check if odd"):
+        is_odd = check_if_odd(num)
+        if is_odd:
             st.write(f"{num} is an odd number.")
+        else:
+            st.write(f"{num} is an even number.")
 
 if __name__ == "__main__":
     main()
