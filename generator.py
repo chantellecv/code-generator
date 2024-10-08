e53ca9f82e40639ee13642fd1ba28bfb7b68d8f5
Chantelle Osafo <84527073+chantellecv@users.noreply.github.com>
2024-10-08 13:54:04 +0000

    Update file

--- generator.py
+++ generator.py
@@ -3,14 +3,10 @@ def app():
     st.title("Is the number odd?")
     number = st.number_input("Enter a number: ")
 
-    while True:
-        if number % 2 != 0:
-            st.write(f"{number} is odd")
-            break
-        else:
-            st.write(f"{number} is not odd, please try again.")
-            number = int(st.text_input("Enter a new number: ", value=str(number)))
+    if number % 2 != 0:
+        st.write(f"{number} is odd")
+    else:
+        st.write(f"{number} is even")
 
-    st.write("Experiment complete, thank you!")
 if __name__ == "__main__":
     app()
