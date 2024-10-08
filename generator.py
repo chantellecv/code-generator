4d381785375da9947eb086b8873a4f87bd096d26
Chantelle Osafo <84527073+chantellecv@users.noreply.github.com>
2024-10-08 11:11:00 +0000

    Update file

--- generator.py
+++ generator.py
@@ -1,16 +1,16 @@
 import streamlit as st
 
-def even_numbers():
-    st.title("Even Number Finder")
+def odd_numbers():
+    st.title("Odd Number Finder")
     numbers = st.text_input("Enter a list of numbers (separated by ','): ")
     if numbers:
         try:
             numbers = list(map(int, numbers.split(',')))
-            even_numbers = [num for num in numbers if num % 2 == 0]
-            st.write("Even numbers are: ")
-            st.write(even_numbers)
+            odd_numbers = [num for num in numbers if num % 2 != 0]
+            st.write("Odd numbers are: ")
+            st.write(odd_numbers)
         except ValueError:
             st.write("Invalid input. Please enter a list of integers separated by commas.")
 
 if __name__ == '__main__':
-    even_numbers()
+    odd_numbers()
