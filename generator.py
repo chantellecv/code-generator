c33740927541ee563a64faa5d5d92b62a4ece8ca
Chantelle Osafo <84527073+chantellecv@users.noreply.github.com>
2024-10-08 13:15:17 +0000

    Update file

--- generator.py
+++ generator.py
@@ -3,14 +3,15 @@ import streamlit as st
 # Create a Streamlit app
 st.title("Odd Number Checker")
 
-# Create a unique key for the number input
-key = "number_input"
-
 # Get the input number from the user
-num = st.number_input("Number", min_value=0, key=key)
+number_input = st.text_input("Enter a number")
 
 # Check if the number is odd
-if num % 2 != 0:
-    st.write(f"{num} is an odd number.")
+if number_input:
+    num = int(number_input)
+    if num % 2 != 0:
+        st.write(f"{num} is an odd number.")
+    else:
+        st.write(f"{num} is an even number.")
 else:
-    st.write(f"{num} is an even number.")
+    st.write("Please enter a number.")
