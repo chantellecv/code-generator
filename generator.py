3347aa1568fe8958aafbc973855cfc049b879678
Chantelle Osafo <84527073+chantellecv@users.noreply.github.com>
2024-10-08 11:38:19 +0000

    Update file

--- generator.py
+++ generator.py
@@ -1,16 +1,29 @@
 import streamlit as st
+import pandas as pd
 
-def odd_numbers():
-    st.title("Odd Number Finder")
-    numbers = st.text_input("Enter a list of numbers (separated by ','): ")
-    if numbers:
-        try:
-            numbers = list(map(int, numbers.split(',')))
-            odd_numbers = [num for num in numbers if num % 2 != 0]
-            st.write("Odd numbers are: ")
-            st.write(odd_numbers)
-        except ValueError:
-            st.write("Invalid input. Please enter a list of integers separated by commas.")
+# Data
+data = {
+    "Category": ["IT", "HR", "Marketing", "Sales", "IT", "HR", "Marketing", "Sales"],
+    "Department": ["Software Development", "Recruitment", "Digital Marketing", "Business Development", "Networking", "Employee Relations", "Content Creation", "Sales Support"],
+    "Priority": [1, 2, 3, 4, 5, 6, 7, 8]
+}
 
-if __name__ == '__main__':
-    odd_numbers()
+df = pd.DataFrame(data)
+
+# Function to find the best department
+def find_best_department(df, category):
+    best_department = df.loc[(df["Category"] == category)].sort_values(by='Priority').head(1)
+    return best_department["Department"].values[0]
+
+# Create Streamlit App
+st.title("Department Finder")
+
+# Get user input
+category = st.selectbox("Select a category", sorted(df["Category"].unique()), key=("SELECT_CATEGORY"))
+
+# Check if the input is valid
+if category:
+    best_department = find_best_department(df, category)
+    st.write(f"The best department for {category} is {best_department}")
+else:
+    st.write("Please select a category.")
