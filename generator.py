8893331093c0f2bdc490928d3fca61ef4bfc387e
Chantelle Osafo <84527073+chantellecv@users.noreply.github.com>
2025-04-04 12:51:07 +0000

    Update file

--- generator.py
+++ generator.py
@@ -7,14 +7,17 @@ def check_anagrams(word1, word2):
     else:
         return False
 
-st.title('Anagram Checker')
+def main():
+    st.title('Anagram Checker App')
+    
+    word1 = st.text_input('Enter the first word:')
+    word2 = st.text_input('Enter the second word:')
+    
+    if st.button('Check Anagrams'):
+        if check_anagrams(word1, word2):
+            st.write(f'{word1} and {word2} are anagrams!')
+        else:
+            st.write(f'{word1} and {word2} are not anagrams.')
 
-word1 = st.text_input('Enter the first word:')
-word2 = st.text_input('Enter the second word:')
-result = check_anagrams(word1, word2)
-
-if st.button('Check Anagrams'):
-    if result:
-        st.write(f'{word1} and {word2} are anagrams!')
-    else:
-        st.write(f'{word1} and {word2} are not anagrams.')
+if __name__ == '__main__':
+    main()
