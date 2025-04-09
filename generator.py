70ed66f65935c26b3dff2b863fddfc972b6c29af
Chantelle Osafo <84527073+chantellecv@users.noreply.github.com>
2025-04-09 11:51:49 +0000

    Update file

--- generator.py
+++ generator.py
@@ -1,24 +1,10 @@
 import streamlit as st
 
-st.title('Simple Calculator')
+st.title('Sum Calculator')
 
-num1 = st.number_input('Enter the first number:')
-num2 = st.number_input('Enter the second number:')
+number1 = st.number_input('Enter the first number:')
+number2 = st.number_input('Enter the second number:')
 
-operation = st.selectbox('Select an operation', ['Addition', 'Subtraction', 'Multiplication', 'Division'])
+sum_result = number1 + number2
 
-result = 0
-
-if operation == 'Addition':
-    result = num1 + num2
-elif operation == 'Subtraction':
-    result = num1 - num2
-elif operation == 'Multiplication':
-    result = num1 * num2
-elif operation == 'Division':
-    if num2 != 0:
-        result = num1 / num2
-    else:
-        st.warning("Division by zero is not allowed. Please enter a non-zero value for the second number.")
-
-st.write(f'Result: {result}')
+st.write(f'The sum of {number1} and {number2} is: {sum_result}')
